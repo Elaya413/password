8a5db8580fe16f8d64f98818e966d86dfac6d3ad
Elaya413 <elayavathiselvam@gmail.com>
2023-09-23 04:40:22 +0530

    express

--- Routes/index.route.js
+++ Routes/index.route.js
@@ -1,7 +1,7 @@
 const {signUpController,resetPasswordRequestController,resetPasswordController,} = require("../Controllers/auth.controller");
   
-  const router = express.Router()
-  
+const router = require("express").Router();
+
   router.post("/auth/signup", signUpController);
   router.post("/auth/requestResetPassword", resetPasswordRequestController);
   router.post("/auth/resetPassword", resetPasswordController);
